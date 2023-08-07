a464630893180dbd46dac85da148e66326a39302
Anup Kumar <anup.rulez@gmail.com>
2023-08-07 16:44:25 +0200

    fix sorting

--- tools/sklearn/ml_visualization_ex.py
+++ tools/sklearn/ml_visualization_ex.py
@@ -24,10 +24,15 @@ from tensorflow.keras.utils import plot_model
 
 from tensorflow.keras.models import model_from_json
 from sklearn.feature_selection._base import SelectorMixin
-from tensorflow.keras.utils import plot_model
-from sklearn.metrics import (auc, average_precision_score,
-                             precision_recall_curve, roc_curve)
+from sklearn.metrics import (
+    auc,
+    average_precision_score,
+    precision_recall_curve,
+    roc_curve,
+)
 from sklearn.pipeline import Pipeline
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.utils import plot_model
 
 safe_eval = SafeEval()
 
